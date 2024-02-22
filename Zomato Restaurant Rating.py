cc5d924be47a9524a048cba598741f2742b7a0b2
Mitesh Gupta <mgmiteshgupta134@gmail.com>
2024-02-22 13:18:19 +0530

    Final Commit

--- Zomato Restaurant Rating.py
+++ Zomato Restaurant Rating.py
@@ -1,18 +1,24 @@
+# Importing Required Libraries
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import pickle
 
-data=pd.read_csv("E:\Data Science\ML Projects\Zomato Restaurant Rating\zomato_data.csv")
+# Loading Dataset
+data=pd.read_csv("zomato_data.csv")
 
+# Splitting the data
 x=data.drop('rate',axis=1)
 y=data['rate']
 
 x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.3,random_state=2)
 
-model=pickle.load(open('E:\Data Science\ML Projects\Zomato Restaurant Rating\model.pkl','rb'))
+# Loading Model
+model=pickle.load(open('model.pkl','rb'))
 
+# Training the model
 model.fit(x_train,y_train)
 
 y_pred=model.predict(x_test)
 
+# Prediction
 print(y_pred)
